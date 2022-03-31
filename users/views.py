d6e8a97325d6e2129e59bc4ade623073291718d8
CoderParth <82562466+CoderParth@users.noreply.github.com>
2022-03-31 12:02:17 +1100

    Update views.py

--- users/views.py
+++ users/views.py
@@ -5,19 +5,22 @@ from django.contrib import messages
 from .models import Profile
 #import login_required decorator for profile
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+
 def register(request):
 	if request.method == 'POST':
 		form = UserRegisterForm(request.POST)
 		if form.is_valid():
+			form.save()
 			username = form.cleaned_data.get('username')
 			messages.success(request, f'Account was created for {username}')
-			form.save()
 			return redirect('login')
+		else:
+			messages.warning(request, 'Something went wrong. Please make sure you satisfy password conditions')
+			form = UserRegisterForm()
+			return render(request, 'users/register.html', {'form': form})
 	else:
 		form = UserRegisterForm()
 		return render(request, 'users/register.html', {'form': form})
-
 @login_required
 def profile(request):
 	return render(request, 'users/profile.html')
